8204f8f73ca7c01930979f6e1d1de1baaa0c2f8a
gauravdal <gaurav.bamania@dal.ca>
2019-02-15 15:24:21 -0400

    task1A.py

---
+++ task1A.py
@@ -0,0 +1,23 @@
+import string
+
+def read_by_words(*args):
+	list1 = []
+	for arg in args:
+		list1.append(unique_words(arg))
+	return type(list1)
+	
+def unique_words(book1):
+	d1 = dict()
+	list1 =[]
+	fout = open(book1, "r")
+	for line in fout:
+		line = line.split()
+		for index in range(0, len(line)):
+			word = line[index].strip(string.whitespace + string.punctuation)
+			d1[word] = d1.get(word, 0) + 1
+			#gauravbamania
+	for word in d1.keys():
+		list1.append(word)
+	return list1 
+
+print(read_by_words("Book1.txt","Book2.txt","Book3.txt"))
