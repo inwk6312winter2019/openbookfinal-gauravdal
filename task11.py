793d0511b57bdbce463fffef71700874faeda6c1
gauravdal <gaurav.bamania@dal.ca>
2019-02-15 13:48:23 -0400

    uploading task11 at git hub

---
+++ task11.py
@@ -0,0 +1,16 @@
+import string
+def unique_words(book1):
+	d1 = dict()
+	list1 =[]
+	fout = open(book1, "r")
+	for line in fout:
+		line = line.split()
+		for index in range(0, len(line)):
+			word = line[index].strip(string.whitespace + string.punctuation)
+			d1[word] = d1.get(word, 0) + 1
+			#gauravbamania
+	for word in d1.keys():
+		list1.append(word)
+	print(list1) 
+
+unique_words("Book1.txt")
